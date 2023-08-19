7cc79101ce2dc66fcf97a76ed6493fa367a59389
parjanyaacoder <padityashukla@gmail.com>
2023-08-19 21:14:36 +0530

    Added react-native-gesture-handler library to fix error

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import {
   View
 } from 'react-native';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import StackNavigator from './configs/StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
 const App = () =>  {
   
   return (
+    <GestureHandlerRootView>
       <NavigationContainer>
               <View style={{ height: "100%", width: "100%", }} >
               <StackNavigator />
               </View>
       
       </NavigationContainer>
+      </GestureHandlerRootView>  
   );
 }
 
